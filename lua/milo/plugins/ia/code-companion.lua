f92a961c3d7220dc499c3bf8c2cf8163c96256a7
TomQuintana <tomquintana20@gmail.com>
2025-06-20 14:46:55 -0300

    feat(plugins): refactor code-companion config to use table entry

--- lua/milo/plugins/ia/code-companion.lua
+++ lua/milo/plugins/ia/code-companion.lua
@@ -1,24 +1,10 @@
--- Adds a comment to the function below
-
 return {
-  "olimorris/codecompanion.nvim",
-  opts = {},
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    --"OXY2DEV/markview.nvim"
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
   },
-  config = function()
-    require("codecompanion").setup({
-      adapters = {
-        copilot_lua = { -- Corregido de 'copilot' a 'copilot_lua'
-          -- No se necesita configuración específica del adaptador si se usa la configuración por defecto de copilot.lua
-        },
-      },
-      -- Puedes añadir keymaps aquí si quieres
-      -- keymaps = {
-      --   { "n", "<C-g>", "<cmd>CodeCompanionToggle<CR>", desc = "Toggle Code Companion" },
-      -- },
-    })
-  end,
 }
